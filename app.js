8190e9a4e8dd92cc3896b721d2dde08869d5407a
pjuang <alberth235689@gmail.com>
2025-10-07 21:16:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,18 @@
-var nextBtn = document.querySelector('.next'),
-    prevBtn = document.querySelector('.prev'),
-    carousel = document.querySelector('.carousel'),
-    list = document.querySelector('.list'), 
-    item = document.querySelectorAll('.item'),
-    runningTime = document.querySelector('.carousel .timeRunning') 
+const slides = document.querySelectorAll('.item');
+const carousel = document.querySelector('.list');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+let index = 0;
 
-let timeRunning = 3000 
-let timeAutoNext = 7000
+function showSlide(i) {
+  if (i < 0) index = slides.length - 1;
+  else if (i >= slides.length) index = 0;
+  else index = i;
 
-nextBtn.onclick = function(){
-    showSlider('next')
+  carousel.style.transform = `translateX(-${index * 100}%)`;
 }
 
-prevBtn.onclick = function(){
-    showSlider('prev')
-}
+nextBtn.addEventListener('click', () => showSlide(index + 1));
+prevBtn.addEventListener('click', () => showSlide(index - 1));
 
-let runTimeOut 
-
-let runNextAuto = setTimeout(() => {
-    nextBtn.click()
-}, timeAutoNext)
-
-
-function resetTimeAnimation() {
-    runningTime.style.animation = 'none'
-    runningTime.offsetHeight /* trigger reflow */
-    runningTime.style.animation = null 
-    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
-}
-
-
-function showSlider(type) {
-    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
-    if(type === 'next'){
-        list.appendChild(sliderItemsDom[0])
-        carousel.classList.add('next')
-    } else{
-        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
-        carousel.classList.add('prev')
-    }
-
-    clearTimeout(runTimeOut)
-
-    runTimeOut = setTimeout( () => {
-        carousel.classList.remove('next')
-        carousel.classList.remove('prev')
-    }, timeRunning)
-
-
-    clearTimeout(runNextAuto)
-    runNextAuto = setTimeout(() => {
-        nextBtn.click()
-    }, timeAutoNext)
-
-    resetTimeAnimation() // Reset the running time animation
-}
-
-// Start the initial animation 
-resetTimeAnimation()
-
+setInterval(() => showSlide(index + 1), 5000);
